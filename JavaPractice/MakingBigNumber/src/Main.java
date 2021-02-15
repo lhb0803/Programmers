c6ab4da3e68567d070bbf60c44323b7c2860994d
leeeeeehyob <lhb0803@snu.ac.kr>
2021-02-15 12:48:31 +0900

    21.02.15.

--- JavaPractice/MakingBigNumber/src/Main.java
+++ JavaPractice/MakingBigNumber/src/Main.java
@@ -14,10 +14,10 @@ public class Main {
         InputExample input6 = new InputExample("10000", 4);
         InputExample input7 = new InputExample("87654321", 3);
         String fuckingLongString_in = "";
-//        for (int i=0; i<50000; i++) {
-//            fuckingLongString_in += "18";
-//        }
-        InputExample input8 = new InputExample("181818", 3);
+        for (int i=0; i<50000; i++) {
+            fuckingLongString_in += "18";
+        }
+        InputExample input8 = new InputExample(fuckingLongString_in, 50000);
         inputList.add(input1); inputList.add(input2); inputList.add(input3); inputList.add(input4);
         inputList.add(input5); inputList.add(input6); inputList.add(input7); inputList.add(input8);
 
@@ -28,17 +28,17 @@ public class Main {
         String output5 = "211";
         String output6 = "1";
         String output7 = "87654";
-        String output8 = "888";
-//        for (int i=0; i<50000; i++) {
-//            output8 += "8";
-//        }
+        String output8 = "";
+        for (int i=0; i<50000; i++) {
+            output8 += "8";
+        }
 
         outputList.add(output1); outputList.add(output2); outputList.add(output3); outputList.add(output4);
         outputList.add(output5); outputList.add(output6); outputList.add(output7); outputList.add(output8);
 
         int numOfCases = inputList.size();
 
-        for (int i=7; i < numOfCases; i++) {
+        for (int i=0; i < numOfCases; i++) {
             System.out.print("case " + (i+1) + ": ");
             InputExample input_i = inputList.get(i);
             printOX(sol.solution(input_i.number, input_i.k), outputList.get(i));
--- JavaPractice/MakingBigNumber/src/Solution.java
+++ JavaPractice/MakingBigNumber/src/Solution.java
@@ -1,32 +1,42 @@
+import java.util.*;
+
 class Solution {
     public String solution(String number, int k) {
+        Stack<Character> answerStack = new Stack<>();
+        Queue<Character> numberQue = new LinkedList<>();
 
-        int i = 0;
-        while(k > 0) {
-            char cAti = number.charAt(i);
-            char cAtiPlusOne = number.charAt(i+1);
-
-            if (cAti < cAtiPlusOne) {
-                number = number.substring(0, i) + number.substring(i+1);
-                k--;
-
-                if ((i > 0) && (number.charAt(i-1) < number.charAt(i))) {
-                    i--;
-                }
-                if (i < 0) {
-                    i = 0;
-                }
-            }
-            else {
-                i++;
-            }
-
-            if (i == number.length()-1) {
-                number = number.substring(0, number.length()-k);
-                k = 0;
-            }
+        for (int n=0; n<number.length(); n++) {
+            numberQue.add(number.charAt(n));
         }
 
-        return number;
+        answerStack.add(numberQue.poll());
+        while(k > 0) {
+            if (!numberQue.isEmpty()) {
+                char c = numberQue.poll();
+                while (!answerStack.isEmpty() && answerStack.peek() < c) {
+                    answerStack.pop();
+                    k--;
+                    if (k <=0 ) {
+                        break;
+                    }
+                }
+                answerStack.add(c);
+            }
+
+            else {
+                for (int i=0; i<k; i++) {
+                    answerStack.pop();
+                }
+                k=0;
+            }
+        }
+        answerStack.addAll(numberQue);
+
+        String answer = "";
+        for (int n=0; n<answerStack.size(); n++) {
+            answer += answerStack.get(n);
+        }
+
+        return answer;
     }
 }

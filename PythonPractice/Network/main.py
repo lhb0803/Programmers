3204a52799afb4675ebf28f823bcf694a218efe4
HyoBae Lee <lhb0803@snu.ac.kr>
2021-05-09 03:56:54 +0900

    21.05.09.
    practice DFS

--- PythonPractice/Network/main.py
+++ PythonPractice/Network/main.py
@@ -1,39 +1,26 @@
 def solution(n, computers):
+    checked = set()
     answer = 0
-    net_stack = list()
-    
-    checked = dict()
-    for i in range(n):
-        checked[i] = False
-    
-    for i, computer in enumerate(computers):
-        if not checked[i]:
-            checked[i] = True
-            net_stack.append((i, computer))
-            
-            c = 1
-            while len(net_stack) > 0:
-                j = net_stack[-1][0]+c
-                if j >= n: # there is no connected computer
-                    temp = net_stack.pop()[0] 
-                    if len(net_stack) > 0:
-                        c = temp - net_stack[-1][0] + 1
-                    continue
-                
-                if net_stack[-1][1][j]: # jth computer is connected
-                    checked[j] = True
-                    net_stack.append((j, computers[j]))
-                    c = 1   
-                else: # jth computer is not connected
-                    c += 1
-                    
-        else:
-            continue
-            
-        if len(net_stack) == 0:
-            answer += 1
-            
+
+    for start in range(len(computers)):
+        res, checked = dfs(computers, start, checked)
+        answer += res
+
     return answer
+
+def dfs(computers, start, checked):
+    if start in checked:
+        return 0, checked
+    
+    checked.add(start)
+    neighbors = computers[start]
+
+    for i, connected in enumerate(neighbors):
+        if i == start or not connected: continue
+        if i not in checked:
+            dfs(computers, i, checked)
+    
+    return 1, checked
 
 
 if __name__=='__main__':
